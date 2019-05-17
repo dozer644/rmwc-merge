c3881b0872d311cff677e468f4de5cc9082b56d4
Dunham, Jack <Jack.Dunham@aarons.com>
2019-05-17 15:02:27 -0400

    Primary color and checkbox width default style

--- src/InputsAndControls/Checkbox/presets/0-default.jsx
+++ src/InputsAndControls/Checkbox/presets/0-default.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Checkbox from "../Checkbox";
 
 export default (
-  <Checkbox uxpId="1">
+  <Checkbox uxpId="1" style={{"width": "300px"}}>
     Cookies
   </Checkbox>
 );
