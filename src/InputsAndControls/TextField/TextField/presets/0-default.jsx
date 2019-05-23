003a4a615567901c54bf43f33823b4649bad821a
Dunham, Jack <Jack.Dunham@aarons.com>
2019-05-23 15:24:12 -0400

    moved input from leading icon to trailing icon

--- src/InputsAndControls/TextField/TextField/presets/0-default.jsx
+++ src/InputsAndControls/TextField/TextField/presets/0-default.jsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import TextField from "../TextField";
 
 export default (
-<TextField uxpId="1" withLeadingIcon="search"  label="Search" />
+<TextField uxpId="1" withTrailingIcon="search"  label="Search" />
 );
